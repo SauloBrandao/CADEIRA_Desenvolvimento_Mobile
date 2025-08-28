f05a9da8c184801d24f9d03678c32e65747c75bf
Oluasz <2023211510080@iesp.edu.br>
2025-08-28 20:30:08 -0300

    Criando Estilo

--- MeuPrimeiroApp/src/components/Exercicio1.js
+++ MeuPrimeiroApp/src/components/Exercicio1.js
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const Exercicio1 = ({Nome}) => {
   return (
-    <View>
+    <View style = {styles.container}>
       <Text>Olá {Nome}</Text>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+            justifyContent: 'center',
+            padding: '100'
+    }
+})
 export default Exercicio1
