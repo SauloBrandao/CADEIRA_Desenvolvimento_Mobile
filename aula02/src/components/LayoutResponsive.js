3d1e6cd3d82bab1136871fadf777353db6872767
SauloBrandao <2023211510080@iesp.edu.br>
2025-09-11 11:54:35 -0300

    feat: criando layout responsivo

--- aula02/src/components/LayoutResponsive.js
+++ aula02/src/components/LayoutResponsive.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const LayoutResponsive = () => {
   return (
-    <View>
-      <Text>LayoutResponsive</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Text>Cabeçalho</Text>
+        </View>
+
+    <Text>Conteúdo Principal</Text>
+    <Text>Rodapé</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {flex: 1},
+    header: {flex: 1, backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center'},
+})
 
 export default LayoutResponsive
